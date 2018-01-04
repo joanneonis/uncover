fe3fda860d0d8540c04e86758ddb0589c1aaa494
joanneonis <joanne.onis@hotmail.com>
2018-01-04 16:03:02 +0100

    sound works!!

--- src/app/rounds/answer/sound/sound.component.ts
+++ src/app/rounds/answer/sound/sound.component.ts
@@ -1,33 +1,82 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as RecordRTC from 'recordrtc';
+
+// followed tutorial on medium:
+// https://medium.com/@SumanthShankar/integrate-recordrtc-with-angular-2-typescript-942c9c4ca93f
 
 @Component({
   selector: 'app-sound',
   templateUrl: './sound.component.html',
   styleUrls: ['./sound.component.css']
 })
-export class SoundComponent implements OnInit {
-  @ViewChild('player') canvas: ElementRef;
+export class SoundComponent implements AfterViewInit {
+  @ViewChild('video') video: ElementRef;
+  recordRTC;
+  stream;
 
   constructor() { }
 
-  ngOnInit() {
-    const handleSuccess = function(stream) {
-      const context = new AudioContext();
-      const source = context.createMediaStreamSource(stream);
-      const processor = context.createScriptProcessor(1024, 1, 1);
-
-      source.connect(processor);
-      processor.connect(context.destination);
-
-      processor.onaudioprocess = function(e) {
-        // Do something with the data, i.e Convert this to WAV
-        console.log(e.inputBuffer);
-      };
-    };
-
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-        .then(handleSuccess);
-
+  ngAfterViewInit() {
+      // set the initial state of the video
+      const video: HTMLVideoElement = this.video.nativeElement;
+      video.muted = false;
+      video.controls = true;
+      video.autoplay = false;
   }
 
+  startRecording() {
+    const mediaConstraints = {
+      video: {
+        mandatory: {
+          minWidth: 1280,
+          minHeight: 720
+        }
+      }, audio: true
+    };
+    navigator.mediaDevices
+      .getUserMedia({audio: true, video: false}) // should be mediaConstraints
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+  successCallback(stream: MediaStream) {
+    const options = {
+      mimeType: 'video/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
+      audioBitsPerSecond: 128000,
+      videoBitsPerSecond: 128000,
+      bitsPerSecond: 128000 // if this line is provided, skip above two
+    };
+    this.stream = stream;
+    this.recordRTC = RecordRTC(stream, options);
+    this.recordRTC.startRecording();
+    const video: HTMLVideoElement = this.video.nativeElement;
+    video.src = window.URL.createObjectURL(stream);
+    this.toggleControls();
+  }
+
+  errorCallback() {
+    console.log('error');
+  }
+  toggleControls() {
+    const video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = !video.muted;
+    video.controls = !video.controls;
+    video.autoplay = !video.autoplay;
+  }
+  stopRecording() {
+    const recordRTC = this.recordRTC;
+    recordRTC.stopRecording(this.processVideo.bind(this));
+    const stream = this.stream;
+    stream.getAudioTracks().forEach(track => track.stop());
+    stream.getVideoTracks().forEach(track => track.stop());
+  }
+  processVideo(audioVideoWebMURL) {
+    const video: HTMLVideoElement = this.video.nativeElement;
+    const recordRTC = this.recordRTC;
+    video.src = audioVideoWebMURL;
+    this.toggleControls();
+    const recordedBlob = recordRTC.getBlob();
+    recordRTC.getDataURL(function (dataURL) { });
+  }
+  download() {
+    this.recordRTC.save('video.webm');
+  }
 }
