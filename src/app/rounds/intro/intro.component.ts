86cd14a564521cd838078260c5e79fc62c203fc7
joanneonis <joanne.onis@hotmail.com>
2018-02-20 15:33:36 +0100

    assets links updated

--- src/app/rounds/intro/intro.component.ts
+++ src/app/rounds/intro/intro.component.ts
@@ -193,43 +193,43 @@ ngOnInit() {
 
   switch (this.round) {
     case 0:
-        this.image.src = '/assets/img/parts/resized/Lijst-met-appels.png';
-        this.backImage = '/assets/img/parts/back/Lijst-met-appels.png';
+        this.image.src = 'assets/img/parts/resized/Lijst-met-appels.png';
+        this.backImage = 'assets/img/parts/back/Lijst-met-appels.png';
         this.activeItem = this.obj[0];
         break;
     case 1:
-        this.image.src = '/assets/img/parts/resized/In-oranje-bol.png';
-        this.backImage = '/assets/img/parts/back/In-oranje-bol.png';
+        this.image.src = 'assets/img/parts/resized/In-oranje-bol.png';
+        this.backImage = 'assets/img/parts/back/In-oranje-bol.png';
         this.activeItem = this.obj[1];
         break;
     case 2:
-        this.image.src = '/assets/img/parts/resized/Oester-met-parels.png';
-        this.backImage = '/assets/img/parts/back/Oester-met-parels.png';
+        this.image.src = 'assets/img/parts/resized/Oester-met-parels.png';
+        this.backImage = 'assets/img/parts/back/Oester-met-parels.png';
         this.activeItem = this.obj[2];
         break;
     case 3:
-        this.image.src = '/assets/img/parts/resized/symboliek-hel.png';
-        this.backImage = '/assets/img/parts/back/symboliek-hel.png';
+        this.image.src = 'assets/img/parts/resized/symboliek-hel.png';
+        this.backImage = 'assets/img/parts/back/symboliek-hel.png';
         this.activeItem = this.obj[3];
         break;
     case 4:
-        this.image.src = '/assets/img/parts/resized/Kont-met-bloem.png';
-        this.backImage = '/assets/img/parts/back/Kont-met-bloem.png';
+        this.image.src = 'assets/img/parts/resized/Kont-met-bloem.png';
+        this.backImage = 'assets/img/parts/back/Kont-met-bloem.png';
         this.activeItem = this.obj[4];
         break;
     case 5:
-        this.image.src = '/assets/img/parts/resized/Heks-met-uil.png';
-        this.backImage = '/assets/img/parts/back/Heks-met-uil.png';
+        this.image.src = 'assets/img/parts/resized/Heks-met-uil.png';
+        this.backImage = 'assets/img/parts/back/Heks-met-uil.png';
         this.activeItem = this.obj[5];
         break;
     case 6:
-        this.image.src = '/assets/img/parts/resized/Ei-en-schelp.png';
-        this.backImage = '/assets/img/parts/back/Ei-en-schelp.png';
+        this.image.src = 'assets/img/parts/resized/Ei-en-schelp.png';
+        this.backImage = 'assets/img/parts/back/Ei-en-schelp.png';
         this.activeItem = this.obj[6];
         break;
     case 7:
-        this.image.src = '/assets/img/parts/resized/Glazen-bol-bouwwerk.png';
-        this.backImage = '/assets/img/parts/back/Glazen-bol-bouwwerk.png';
+        this.image.src = 'assets/img/parts/resized/Glazen-bol-bouwwerk.png';
+        this.backImage = 'assets/img/parts/back/Glazen-bol-bouwwerk.png';
         this.activeItem = this.obj[7];
   }
 }
@@ -238,7 +238,7 @@ ngOnInit() {
     if (!this.canvas) {
       return;
     }
-    this.brush.src = '/assets/img/brush.png';
+    this.brush.src = 'assets/img/brush.png';
     this.ctx = this.canvas.nativeElement.getContext('2d');
 
     const that = this;
